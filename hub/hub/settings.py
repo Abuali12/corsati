1d59413369a93d9984adf92fab02e5f7bc138af6
Abuali12 <hassan.mohemmad777@gmail.com>
2026-04-12 14:55:54 +0200

    map ux

--- hub/hub/settings.py
+++ hub/hub/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 
 SITE_ID= int(os.environ.get('SITE_ID', 1)) 
 
-DEBUG= os.environ.get('DEBUG','True') ==  'True'
+DEBUG= os.environ.get('DEBUG','False') ==  'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'course-hub-5tic.onrender.com']
 
