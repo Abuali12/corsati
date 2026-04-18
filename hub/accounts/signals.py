00f498d19151276155a5bff56eb59e4cda1b771d
Abuali12 <hassan.mohemmad777@gmail.com>
2026-04-18 10:28:56 +0200

    debugg to false, and social account signup signel

--- hub/accounts/signals.py
+++ hub/accounts/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from allauth.account.signals import user_signed_up
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from core.models import Profile
@@ -6,4 +7,8 @@ from core.models import Profile
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.get_or_create(user=instance)
+
+@receiver(user_signed_up)
+def create_profile_social(request, user, **kwargs):
+    Profile.objects.get_or_create(user=user)
--- hub/centers/apps.py
+++ hub/centers/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class CentersConfig(AppConfig):
     name = 'centers'
-
-
-    def ready(self):
-        import centers.signals
